97a90c3e80c15748c204318262cab969fb471e8f
Dirk Heinke <dirkheinke@gmail.com>
2021-09-09 22:55:16 +0200

    Fancy Matrix Code
    Co-authored-by: johnkrmr <johnkrmr@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Notizen
+
+## Idee
+
+- QRCode
+- Jumper der zwischen Buttons und Fototransistor wechselt
+    - Fototransistor kann dann genutzt werden, um die Zeit von einer Webseite per Flickercode zu übertragen
+- vllt Transistor woanders dran machen (Matrix oder I2C)
+
 ## Software
 - https://forum.arduino.cc/t/time-libary-sommerzeit-winterzeit/221884
+
+
+## ADC Calculation
+| No. Switches    |           3 |        | Button No. | Req'd ADC | Req'd V (R1) | Req'd V (R2+) | Req'd R | Closest R | Actual V (R1) | Actual V (R2+) | Actual ADC | Diff from prev. | In Tolerance | Current (mA) |
+|-----------------|------------:|--------|-----------:|----------:|-------------:|--------------:|--------:|----------:|--------------:|---------------:|-----------:|----------------:|-------------:|-------------:|
+| ADC Resolution  |        1024 |        |          0 |      1024 |         0.00 |          5.00 |   47000 |     47000 |          5.00 |           0.00 |          0 |               0 |          N/A |         0.11 |
+| Tolerance (%)   |           2 |        |          1 |       341 |         3.33 |          1.67 |   23500 |     24000 |          3.31 |           1.69 |        346 |             346 |          YES |         0.07 |
+| Voltage         |           5 |        |          2 |       683 |         1.67 |          3.33 |   70500 |     68000 |          1.69 |           3.31 |        678 |             332 |          YES |         0.04 |
+|                 |             |        |          3 |           |              |               |         |           |               |                |            |                 |              |              |
+|                 |             |        |          4 |           |              |               |         |           |               |                |            |                 |              |              |
+|                 |             |        |          5 |           |              |               |         |           |               |                |            |                 |              |              |
+|                 |             |        |          6 |           |              |               |         |           |               |                |            |                 |              |              |
+|                 |             |        |          7 |           |              |               |         |           |               |                |            |                 |              |              |
+| ADC Tolerance   |       20.48 |  (+/-) |          8 |           |              |               |         |           |               |                |            |                 |              |              |
+| Std. Deviation  | 10.27164132 |        |          9 |           |              |               |         |           |               |                |            |                 |              |              |
+|                 |             |        |         10 |           |              |               |         |           |               |                |            |                 |              |              |
+|                 |             |        |         11 |           |              |               |         |           |               |                |            |                 |              |              |
--- firmware/usb_binary_clock_test2/src/main.cpp
+++ firmware/usb_binary_clock_test2/src/main.cpp
@@ -1,7 +1,14 @@
+/*
+USB Binary Clock using ATtin84 and MCP7940
+
+Notes:
+timer interrupt generator: https://www.arduinoslovakia.eu/application/timer-calculator
+*/
+
 #include <Arduino.h>
 #include <MCP7940.h>
 
-// pinout for pio (useing alternative pinout)
+// pinout for pio https://raw.githubusercontent.com/SpenceKonde/ATTinyCore/master/avr/extras/Pinout_x4.jpg
 #define ROW0 10
 #define ROW1 9
 #define ROW2 8
@@ -10,12 +17,41 @@
 #define COL1 0
 #define COL2 1
 #define COL3 2
+#define BUTTON_PIN A5
+#define BUTTON_1_THRESHOLD 173 // 0
+#define BUTTON_2_THRESHOLD 512 // 346
+#define BUTTON_3_THRESHOLD 851 // 678
 
-const int COL[] = {COL0, COL1, COL2, COL3};
-const int ROW[] = {ROW0, ROW1, ROW2, ROW3};
+#define MCP_TIMER_INTERVAL_MS 1000
+
+int const COL[] = {COL0, COL1, COL2, COL3};
+int const ROW[] = {ROW0, ROW1, ROW2, ROW3};
+
+uint8_t columnCounter = 0;
+
+uint8_t columnValues[4] = {0};
 
 // MCP7940 object
 MCP7940_Class MCP7940;
+
+void setupTimer1()
+{
+  noInterrupts();
+  // Clear registers
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+
+  // 1000 Hz (8000000/((124+1)*64))
+  OCR1A = 124;
+  // CTC
+  TCCR1B |= (1 << WGM12);
+  // Prescaler 64
+  TCCR1B |= (1 << CS11) | (1 << CS10);
+  // Output Compare Match A Interrupt Enable
+  TIMSK1 |= (1 << OCIE1A);
+  interrupts();
+}
 
 void setup()
 {
@@ -27,6 +63,8 @@ void setup()
   pinMode(COL1, OUTPUT);
   pinMode(COL2, OUTPUT);
   pinMode(COL3, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  setupTimer1();
 
   while (!MCP7940.begin())
   {
@@ -36,44 +74,78 @@ void setup()
   MCP7940.adjust();
 }
 
-// set column col according to binary patter row_values
-void setCol(uint8_t col, uint8_t row_values)
+// set column col according to binary patter columnValue
+void setCol(uint8_t col, uint8_t columnValue)
 {
   digitalWrite(COL0, HIGH);
   digitalWrite(COL1, HIGH);
   digitalWrite(COL2, HIGH);
   digitalWrite(COL3, HIGH);
 
-  digitalWrite(ROW0, row_values & 0b0001);
-  digitalWrite(ROW1, row_values & 0b0010);
-  digitalWrite(ROW2, row_values & 0b0100);
-  digitalWrite(ROW3, row_values & 0b1000);
+  digitalWrite(ROW0, columnValue & 0b0001);
+  digitalWrite(ROW1, columnValue & 0b0010);
+  digitalWrite(ROW2, columnValue & 0b0100);
+  digitalWrite(ROW3, columnValue & 0b1000);
 
   digitalWrite(COL[col], LOW);
 }
 
-// 0b1 ->
-// 0b1 -> 0000 0001
-
-void loop()
+void getTimeAndWriteToLeds()
 {
-  /*setCol(0, 2);
-  setCol(1, 1);
-  setCol(2, 4);
-  setCol(3, 1);*/
-  //setCol(0, 0b0000);
-  //delay(10);
-
   static uint8_t secs;          // store the seconds value
   DateTime now = MCP7940.now(); // get the current time
   if (secs != now.second())
   { // Output if seconds have changed
     //  now.hour();
     //  now.minute();
-    // TODO Zeit in Variable schreiben und setCol im Interrupt setzen
+    columnValues[3] = now.second() % 10;
+    columnValues[2] = now.second() / 10;
+    columnValues[1] = now.minute() % 10;
+    columnValues[0] = now.minute() / 10;
+    secs = now.second();
   }
-  setCol(3, now.second() % 10);
-  delay(5);
-  setCol(2, now.second() / 10);
-  secs = now.second(); // Set the counter variable
+}
+
+uint8_t getButtons()
+{
+  uint16_t read = analogRead(BUTTON_PIN);
+  if (read < BUTTON_1_THRESHOLD)
+  {
+    return 1;
+  }
+  if (read < BUTTON_2_THRESHOLD) 
+  {
+    return 2;
+  }
+  if (read < BUTTON_3_THRESHOLD)
+  {
+    return 3;
+  }
+  return 0; // Kein Button gedrückt
+}
+
+void loop()
+{
+  unsigned long static tLastUpdateTimer = 0;
+  unsigned long tCurrentTime = millis();
+
+
+  uint8_t readButtons = getButtons();
+  columnValues[0] = readButtons;
+
+  // if (tCurrentTime - tLastUpdateTimer > MCP_TIMER_INTERVAL_MS)
+  // {
+  //   getTimeAndWriteToLeds();
+  //   tLastUpdateTimer = tCurrentTime;
+  // }
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  setCol(columnCounter, columnValues[columnCounter]);
+  ++columnCounter;
+  if (columnCounter == 4)
+  {
+    columnCounter = 0;
+  }
 }
